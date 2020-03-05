3ea17113bb259e7587aa93d1f6f65084e34c28f5
nago3579 <nago3579@colorado.edu>
2020-03-05 12:13:39 -0700

    the app

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render () {
+    return (
+    <div>
+      <div id="table">
+        <div class = "topbar">≡ ATLS2200-010 &#62; Assignments &#62;</div>
+        <div class = "topbar2">Lab Assignment: CSS Grid Emulation</div>
+        <div class = "aside1" id = "aside"> Home Syllabus Announcements </div>
+        <div class = "aside2" id = "aside"> Assignments Lectures Grades</div>
+        <div class = "aside3" id = "aside"> People Collaborations Chat</div>
+        <div class = "aside4" id = "aside"> Modules Conferences </div>
+        <div class = "aside5" id = "aside"> Name Coach My Media </div>
+        <div class = "line"><hr/></div>
+        <div class = "sidebar1" id = "sidebar">logo</div>
+        <div class = "sidebar2" id = "sidebar">account</div>
+        <div class = "sidebar3" id = "sidebar">dashboard</div>
+        <div class = "sidebar4" id = "sidebar">courses</div>
+        <div class = "sidebar5" id = "sidebar">groups</div>
+        <div class = "sidebar6" id = "sidebar">calendar</div>
+        <div class = "sidebar7" id = "sidebar">inbox</div>
+        <div class = "sidebar8" id = "sidebar">help</div>
+        <div class = "sidebar9" id = "sidebar"></div>
+        <div class = "sidebar0" id = "sidebar">&#8592;</div>
+        <div class = "title"> Lab Assignment: CSS Grid Emulation BUTTON </div>
+        <div class = "line2"><hr/></div>
+        <div class = "pointsstuff"> Due Dates and stuff. Point 10. This took lots of effort. Not sure if it was worth it. </div>
+        <div class = "line3"><hr/></div>
+        <div class = "listheader"> Text text text text </div>
+        <div class = "list"> <ul><li>You gets whats going on here. Mouse over the icons</li></ul></div>
+      </div>
+    </div>
+    )
+  }
+}
+
+export default App;
